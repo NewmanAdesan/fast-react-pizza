59b1a6b7b6299b130c11c50c57f55a6c523eb510
NewmanAdesan <newmanadesan@gmail.com>
2024-01-20 16:04:32 +0100

    Implemented a Validation for the phone-number entry of the order form

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect } from 'react-router-dom'
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -33,6 +33,9 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting"
+  const formErrors = useActionData();
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
@@ -51,6 +54,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -73,7 +77,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? 'Placing order...' : 'Order now'}
+          </button>
         </div>
       </Form>
     </div>
@@ -81,15 +87,27 @@ function CreateOrder() {
 }
 
 export async function action({request}){
+
+  // get the form data from the request 
   let formData = await request.formData();
+
+  // transform the form data into an object
   formData = Object.fromEntries(formData);
 
+  // process values of the form data into a required structure
   let order = {
     ...formData,
     priority: formData.priority === 'on',
     cart: JSON.parse(formData.cart)
   }
 
+  // validate the values in the form data
+  const errors = {}
+  if (!isValidPhone(order.phone)) errors.phone = "Please give us your correct phone number. We might need it to contact you."
+  
+  if (Object.keys(errors).length > 0) return errors;
+
+  // submit form data
   const newOrder = await createOrder(order);
 
   // redirect to the route '/order/:orderId' using a function by react router called 'redirect'
