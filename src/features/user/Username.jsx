0c6c647a6488a155bb21d26f38340860ea78809d
NewmanAdesan <newmanadesan@gmail.com>
2024-01-22 22:41:36 +0100

    Styling the Home-page, the Menu-page and the Cart-page

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,9 @@
+
+
+const Username = () => {
+  return (
+    <div className="text-sm font-semibold hidden md:block">Newman</div>
+  )
+}
+
+export default Username;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,12 +9,12 @@ const AppLayout = () => {
   const navigation = useNavigation();
   const isLoading = navigation.state == 'loading';
   return (
-    <div className='layout'>
+    <div className='grid h-screen grid-rows-[auto_1fr_auto]'>
         {isLoading && <Loader/>}
 
         <Header />
 
-        <main>
+        <main className='overflow-scroll max-w-3xl mx-auto w-full'>
             <Outlet />
         </main>
 
